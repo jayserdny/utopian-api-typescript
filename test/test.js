15900adbeaa683e8f75f8b0124b7a0a0836314aa
Jayser Mendez <jayser.mendez@stu.bcc.cuny.edu>
2018-01-17 06:03:13 -0500

    test file fixed

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 'use strict';
 var expect = require('chai').expect;
-var utopian = require('../build/utopiansdk.js');
+var utopian = require('../build/index.js');
 describe('Utopian api test', () => {
     it('Should return an array', () => {
         return utopian.getModerators().then(function(data) {
